78a0e3d2374ec37aa46a83660b0a4d067c392f36
ChristabelKindrat <christabelkril@gmail.com>
2022-09-30 10:20:20 +0300

    add pagination file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import './components/Pagination';
+
+import {Pagination} from './components/Pagination';
 
 function App() {
-  return (
-    <div>
 
-    </div>
-  );
+    return (
+        <>
+            <Pagination activePage={1} perPage={1} withActions={true} totalItems={5} onChangePage={(newPage: number)=>{}} classes={{btn:'Pagination_container',activeBtn:"active"}}/>
+        </>
+    );
 }
 
 export {App};
